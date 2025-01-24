69cd984b20615bd64973fb19a2e233c28c071005
Gaguchi <gaga9393@gmail.com>
2025-01-24 23:15:54 +0400

    api docs

--- django_multivendor/urls.py
+++ django_multivendor/urls.py
@@ -1,11 +1,14 @@
 # ...existing code...
 from django.urls import path, include
 from django.contrib import admin
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/vendors/', include('vendors.urls')),
     path('api/products/', include('products.urls')),
     # ...other URLs...
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 # ...existing code...
