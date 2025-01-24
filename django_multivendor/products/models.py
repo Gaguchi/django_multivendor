73518181c793b7af7f3cbf98df53044340e5b9da
Gaguchi <gaga9393@gmail.com>
2025-01-24 20:29:49 +0400

    models update

--- django_multivendor/products/models.py
+++ django_multivendor/products/models.py
@@ -1,16 +1,11 @@
 from django.db import models
-# from vendors.models import Vendor  # Remove or comment this out
+from vendors.models import Vendor
 
-# class Product(models.Model):
-#     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, related_name='products')
-#     name = models.CharField(max_length=100)
-#     def product_directory_path(instance, filename):
-#         # file will be uploaded to MEDIA_ROOT/products/<vendor>/<product_name>/<filename>
-#         return f'products/{instance.vendor.name}/{instance.name}/{filename}'
-#
-#     thumbnail = models.ImageField(upload_to=product_directory_path, null=True, blank=True)
-#     image = models.ImageField(upload_to=product_directory_path, null=True, blank=True)
-#     video = models.FileField(upload_to=product_directory_path, null=True, blank=True)
-#
-#     def __str__(self):
-#         return self.name
+class Product(models.Model):
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    # ...add more fields as needed...
+
+    def __str__(self):
+        return self.name
--- django_multivendor/vendors/models.py
+++ django_multivendor/vendors/models.py
@@ -1,37 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.utils import timezone
-# from products.models import Product  # Remove or comment this out if present
 
 class Vendor(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
-    store_name = models.CharField(max_length=100, unique=True, default='')
-    description = models.TextField(blank=True, default='')
-    contact_email = models.EmailField(blank=True, default='')
-    phone = models.CharField(max_length=15, blank=True, default='')
-    address = models.TextField(blank=True, default='')
-    logo = models.ImageField(upload_to='vendor_logos/', blank=True, default='vendor_logos/default_logo.png')
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=100)
+    # ...add more fields as needed...
 
     def __str__(self):
-        return self.store_name
+        return self.name
 
-class VendorProduct(models.Model):
-    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, related_name='vendor_products')
-    name = models.CharField(max_length=100, default='')
-    sku = models.CharField(max_length=50, blank=True, default='')
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    stock = models.PositiveIntegerField(default=0)
-    description = models.TextField(blank=True, default='')
-
-    def product_directory_path(instance, filename):
-        return f'vendor_products/{instance.vendor.store_name}/{instance.name}/{filename}'
-
-    thumbnail = models.ImageField(upload_to=product_directory_path, null=True, blank=True)
-    image = models.ImageField(upload_to=product_directory_path, null=True, blank=True)
-    video = models.FileField(upload_to=product_directory_path, null=True, blank=True)
-    # ...add more product-specific fields as needed...
-
-    def __str__(self):
-        return f"{self.vendor.store_name} - {self.name}"
+# Remove the Product model from here
