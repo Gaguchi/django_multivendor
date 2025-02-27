da89e8e7eb18ee7f565660bb481298b897d8939f
Gaguchi <gaga9393@gmail.com>
2025-02-27 13:25:26 +0400

    renamed folders for convenience

--- backend/django_multivendor/settings.py
+++ backend/django_multivendor/settings.py
@@ -190,7 +190,7 @@ REST_FRAMEWORK = {
 # Add Simple JWT settings
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': True,
